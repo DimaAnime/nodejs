3cd4bc157c963aae566da77c1921a52a2c82b6b1
Dimas <megamix2309@gmail.com>
2025-01-10 13:20:40 +1000

    Добавлен middleware для перезвата ошибки невалидного json в теле запроса и отправки собственного ответа

--- app/server.js
+++ app/server.js
@@ -7,6 +7,18 @@ const app=express();
 
 
 app.use(express.json());
+
+// Middleware для обработки ошибок от express.json()
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+      // Перехватываем ошибку парсинга JSON
+      return res.status(400).json({
+        error: 'Некорректный JSON. Проверьте формат данных.',
+      });
+    }
+    // Передача других ошибок
+    next(err);
+  });
 app.use(router);
 
 
